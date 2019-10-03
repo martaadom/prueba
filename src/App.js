a2350e715171d2f40e24a07af1bd50ed4f71fd8b
marta dominguez <marta.dominguezm@alumnos.upm.es>
2019-10-03 13:21:46 +0200

    primer paso

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {connect} from 'react-redux';
+import Game from "./Game";
+import {questionAnswer} from './redux/actions'
+
+function App(props) {
+  return(
+     <div className={'App'}>
+       <Game question={props.questions[props.currentQuestion]}
+             onQuestionAnswer={(answer)=>{
+               props.dispatch(questionAnswer(props.currentQuestion, answer))
+             }}/>
+     </div>
+  );
+}
+
+function mapStateToProps(state){
+  return{
+    ...state
+
+  };
+}
+export default connect(mapStateToProps)(App);
